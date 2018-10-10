21758f6192aa25f05811e07f11b0da16c42609c7
Justin Rohrman <justin.rohrman@teamnorthwoods.com>
2018-10-10 09:39:08 -0500

    yer a kata, harry

--- lib/rpsls.rb
+++ lib/rpsls.rb
@@ -1,14 +1,32 @@
-require 'pry'
-
 class Rpsls
   def initialize
     @player_1_win = "Player 1 won!"
     @player_2_win = "Player 2 won!"
+    @unknown_thing = "Oh, Unknown Thing"
+    @valid_entries = %w(rock paper scissors spock lizard)
   end
 
-  def fight(player_1, player_2)
-    if (player_1 = "scissors" and (player_2 = "lizard" or player_2 = "paper"))
+  def fight(player_1, player_2) 
+    if (player_1 == "rock" and (player_2 == "lizard" or player_2 == "scissors"))
       @player_1_win
+    elsif (player_1 == "paper" and (player_2 == "rock" or player_2 == "spock"))
+      @player_1_win
+    elsif (player_1 == "scissors" and (player_2 == "lizard" or player_2 == "paper"))
+      @player_1_win
+    elsif (player_1 == "spock" and (player_2 == "rock" or player_2 == "scissors"))
+      @player_1_win
+    elsif (player_1 == "lizard" and (player_2 == "spock" or player_2 == "paper"))
+      @player_1_win
+    else
+      @player_2_win
+    end
+  end
+
+  def is_known(player_1, player_2)
+    if @valid_entries.include?(player_1) and @valid_entries.include?(player_2)
+      fight(player_1, player_2)
+    else
+      @unknown_thing
     end
   end
 end
--- spec/rpsls_spec.rb
+++ spec/rpsls_spec.rb
@@ -1,7 +1,21 @@
-require 'pry'
-
 describe Rpsls do
 
+  it 'crushes lizard' do
+    expect(subject.fight('rock', 'lizard')).to eq("Player 1 won!")
+  end
+
+  it 'crushes scissors' do
+    expect(subject.fight('rock', 'scissors')).to eq("Player 1 won!")
+  end
+
+  it 'covers rock' do
+    expect(subject.fight('paper', 'rock')).to eq("Player 1 won!")
+  end
+
+  it 'disproves spock' do
+    expect(subject.fight('paper', 'spock')).to eq("Player 1 won!")
+  end
+  
   it 'decapitates lizard' do
     expect(subject.fight('scissors', 'lizard')).to eq("Player 1 won!")
   end
@@ -10,5 +24,28 @@ describe Rpsls do
     expect(subject.fight('scissors', 'paper')).to eq("Player 1 won!")
   end
 
+  it 'vaporizes rock' do
+    expect(subject.fight('spock', 'rock')).to eq("Player 1 won!")
+  end
+
+  it 'smashes scissors' do
+    expect(subject.fight('spock', 'scissors')).to eq ("Player 1 won!")
+  end
+
+  it 'poisons spock' do
+    expect(subject.fight('lizard', 'spock')).to eq("Player 1 won!")
+  end
+
+  it 'eats paper' do
+    expect(subject.fight('lizard', 'paper')).to eq("Player 1 won!")
+  end
+
+  it 'player 2 wins' do
+   expect(subject.fight('rock', 'paper')).to eq("Player 2 won!")
+  end
+
+  it 'is unknown' do
+    expect(subject.is_known('fart', 'spock')).to eq("Oh, Unknown Thing")
+  end
 end
 
